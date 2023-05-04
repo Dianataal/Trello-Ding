096341f638ec1b2d7f715baebe88a76280c8d17e
Diana Taal <taaldiana@gmail.com>
2023-05-04 14:11:14 +0300

    almost working trello data watcher

--- Ding.js
+++ Ding.js
@@ -1,16 +1,91 @@
-fetch (`https://api.trello.com/1/boards/${board}/cards?key=${key}&token=${token}`, {
+import config from './config.json';
+
+const key = config.key;
+const token = config.token;
+const board = config.board;
+
+
+let cardMap = {};
+
+function checkChanges() {
+  fetch(`https://api.trello.com/1/boards/${board}/cards?key=${key}&token=${token}`, {
     method: 'GET',
     headers: {
       'Accept': 'application/json'
     }
   })
-    .then(res => {
-      console.log(res);
-      return res.json();
-    })
-    .then(data => {
-        data.forEach(task => {
-          console.log(task.name, task.start, task.due);
-        });
-      })
-      .catch(error => console.log(error));
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(task => {
+      // check if list name includes -top
+      if (task.idList) {
+        fetch(`https://api.trello.com/1/lists/${task.idList}?key=${key}&token=${token}&fields=name`, {
+          method: 'GET',
+          headers: {
+            'Accept': 'application/json'
+          }
+        })
+        .then(res => res.json())
+        .then(list => {
+          if (list.name.includes('-top')) {
+            const topListName = list.name;
+            // check if card exists in cardMap
+            if (cardMap.hasOwnProperty(task.id)) {
+              // card exists, check if list has changed
+              if (cardMap[task.id] !== task.idList) {
+                fetch(`https://api.trello.com/1/lists/${cardMap[task.id]}?key=${key}&token=${token}&fields=name`, {
+                  method: 'GET',
+                  headers: {
+                    'Accept': 'application/json'
+                  }
+                })
+                .then(res => res.json())
+                .then(prevList => {
+                  if (prevList.name.includes('-top')) {
+                    console.log(`Card ${task.name} has been moved from list ${prevList.name} to ${topListName}`);
+                  } else {
+                    console.log(`Card ${task.name} has been added to list ${topListName}`);
+                  }
+                  // new list added
+                  cardMap[task.id] = task.idList;
+                })
+                .catch(error => console.log(error));
+              }
+            } else {
+              // card doesn't exist, add to cardMap
+              cardMap[task.id] = task.idList;
+              console.log(`Card ${task.name} has been added to list ${topListName}`);
+            }
+          } else {
+            // card is not in a list with -top in the name
+            if (cardMap.hasOwnProperty(task.id)) {
+              // card exists, check if it was removed. not working as intended
+              fetch(`https://api.trello.com/1/lists/${cardMap[task.id]}?key=${key}&token=${token}&fields=name`, {
+                method: 'GET',
+                headers: {
+                  'Accept': 'application/json'
+                }
+              })
+              .then(res => res.json())
+              .then(prevList => {
+                if (prevList.name.includes('-top')) {
+                  console.log(`Card ${task.name} has been moved from list ${prevList.name}`);
+                } else {
+                  console.log(`Card ${task.name} has been deleted`);
+                }
+                // this doesn't work properly. maybe archiving makes sth idk?
+                delete cardMap[task.id];
+              })
+              .catch(error => console.log(error));
+            }
+          }
+        })
+        .catch(error => console.log(error));
+      }
+    });
+    console.log('No changes yet'); // if no changes in 10sec says this
+  })
+  .catch(error => console.log(error));
+}
+
+setInterval(checkChanges, 10000);
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 Introduction
 
-Code for a Power-Up in Trello that tracks card movement and can be connected to something to do when the card moves. Probably.
+Code for a Power-Up in Trello that tracks lists with a keyword and the card movement in those. Probably.
+Currently made for this: https://trello.com/b/ZoVZDKTr/testroom It wants to be a Power-Up some day.
+
+Working:
+* Lists with -top data displayed
+* Moving cards from lists with -top to lists without -top shows that card has been moved but doesn't show where
+* Checks every 10sec for new cards in -top lists
+* Moving cards between all lists - from no-top lists show a new card is added, from top to top lists shows card is moved from list name to list name
+* Adding cards to -top lists shows a card added but non-top lists don't
+
+'it's not a bug it's a feature'?:
+* When changing a list name to include -top log will display all cards in that list as they were just added and when removing -top it will log that they were deleted
+> Add bit to log that a new -top list was made?
+
+Bugged:
+* Removing cards (archive) doesn't show anything in log. Neither does deleting them from archive.
+> Get this part working if a card is archived/deleted. Or is it really necessary.....?
